62a8cb9e553634cf09d1a9ac039ffe25fc6c9090
Mashkoor <mashkoorimran3@gmail.com>
2024-10-11 20:55:08 -0700

    intial

---
+++ Practice/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Practice
+{
+    public static void Main(string[] args)
+     {
+        // simple program to print hello world
+        // Console.WriteLine("Hello World");
+
+        /* int a=2;
+          int b = 4;
+           int result= a+b;
+              Console.WriteLine(result);
+              Console.ReadLine();
+         */
+        /*Adding numbers of 2 different datatypes
+        and then converting them into 3rd data type int
+        double d = 2.4;
+        float f = 4;
+         int result = Convert.ToInt32(d + f);
+        Console.WriteLine(result); 
+        */
+        //program for uniary operators to check what happens with prior or later assignment.
+        int  a = 5;
+        Console.WriteLine(++a);
+        Console.ReadLine();
+        Console.WriteLine(a++);
+        Console.ReadLine();
+        Console.WriteLine(a);
+        Console.ReadLine(); 
+
+
+    }
+
+
+}
